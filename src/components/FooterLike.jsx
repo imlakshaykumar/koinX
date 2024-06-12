265da261322874ba912296c730ef78df1acfde26
Lakshay kumar <imlakshaykumar@gmail.com>
2024-06-13 00:29:21 +0530

    fixed footer slide

--- src/components/FooterLike.jsx
+++ src/components/FooterLike.jsx
@@ -64,7 +64,7 @@ export const FooterLike = () => {
         infinite: true,
         speed: 500,
         slidesToShow: 5,
-        slidesToScroll: 5,
+        slidesToScroll: 2,
         initialSlide: 0,
         nextArrow: <SampleNextArrow />,
         prevArrow: <SamplePrevArrow />,
@@ -72,23 +72,23 @@ export const FooterLike = () => {
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToShow: 4,
+                    slidesToScroll: 2,
                     infinite: true,
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToShow: 4,
+                    slidesToScroll: 2,
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3
+                    slidesToScroll: 1
                 }
             }
         ]
